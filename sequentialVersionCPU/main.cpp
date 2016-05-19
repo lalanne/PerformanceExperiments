a1e448327d78d5b713b2b289044c47425037d481
christian.lalanne@zweicom.com <christian.lalanne@zweicom.com>
2016-05-18 23:57:05 -0300

    ADD: construction of a basis using benchmark from google

--- sequentialVersionCPU/main.cpp
+++ sequentialVersionCPU/main.cpp
@@ -1,8 +1,65 @@
 
-#include <iostream>
+#include "benchmark/benchmark_api.h"
+#include <omp.h>
 
-using namespace std;
+#define MAD_4(x, y)     x = (y*x) + y;      y = (x*y) + x;      x = (y*x) + y;      y = (x*y) + x;
+#define MAD_16(x, y)    MAD_4(x, y);        MAD_4(x, y);        MAD_4(x, y);        MAD_4(x, y);
+#define MAD_64(x, y)    MAD_16(x, y);       MAD_16(x, y);       MAD_16(x, y);       MAD_16(x, y);
 
-int main() {
-    cout << "Hello World!" << "\n";
+void fma(int* out, int A, int i) {
+	int x = A;
+	int y = i;
+
+	MAD_64(x, y);   MAD_64(x, y);
+    MAD_64(x, y);   MAD_64(x, y);
+    MAD_64(x, y);   MAD_64(x, y);
+    MAD_64(x, y);   MAD_64(x, y);
+    MAD_64(x, y);   MAD_64(x, y);
+    MAD_64(x, y);   MAD_64(x, y);
+    MAD_64(x, y);   MAD_64(x, y);
+    MAD_64(x, y);   MAD_64(x, y);
+
+    MAD_64(x, y);   MAD_64(x, y);
+    MAD_64(x, y);   MAD_64(x, y);
+    MAD_64(x, y);   MAD_64(x, y);
+    MAD_64(x, y);   MAD_64(x, y);
+    MAD_64(x, y);   MAD_64(x, y);
+    MAD_64(x, y);   MAD_64(x, y);
+    MAD_64(x, y);   MAD_64(x, y);
+    MAD_64(x, y);   MAD_64(x, y);
+
+	out[i] = y;
 }
+
+const int ITERATIONS = 1000000;
+
+void cpuSequentialOperation() {
+    int out[ITERATIONS];
+    for(int i=0; i<ITERATIONS; ++i) fma(out, 4, i);
+}
+
+void cpuParallelOmpOperation() {
+    int out[ITERATIONS];
+#pragma omp parallel for
+    for(int i=0; i<ITERATIONS; ++i) {
+        fma(out, 4, i);
+    }
+}
+
+void cpuSequential(benchmark::State& state) {
+    while (state.KeepRunning()) {
+        cpuSequentialOperation();
+    }
+}
+BENCHMARK(cpuSequential);
+
+void cpuParalleOmp(benchmark::State& state) {
+    while (state.KeepRunning()) {
+        cpuParallelOmpOperation();
+    }
+}
+BENCHMARK(cpuParalleOmp);
+
+BENCHMARK_MAIN();
+
+
